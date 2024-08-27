01d628381f46a664eeb376fcfef98feda854becb
Abdallah Aburomi <abdlaa1912@gmail.com>
2024-08-27 22:56:43 +0300

    removed unnecessary object

--- hw2/src/utils/DataUtils.js
+++ hw2/src/utils/DataUtils.js
@@ -8,17 +8,6 @@ export function getHighest_returnAmount(data, value, returnAmount){
     return sortedData.slice(0,returnAmount)
 }
 
-export const electricityApiData = {
-    'Retail prices': {
-        apiUrl: "https://api.eia.gov/v2/electricity/state-electricity-profiles/summary/data/?api_key=0elCt43O9HAYcRzX9wMXx5euPbU4PBOtw21LgY22&frequency=annual&data[0]=average-retail-price&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000",
-        desc: "Price retails"
-    },
-    'Industry & Commercial Consumption': {
-        apiUrl: "https://api.eia.gov/v2/electricity/state-electricity-profiles/source-disposition/data/?api_key=0elCt43O9HAYcRzX9wMXx5euPbU4PBOtw21LgY22&frequency=annual&data[0]=direct-use&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000",
-        desc: "Industry & Commercial Electricity Consumption"
-    }
-}
-
 export const energyData = {
     'electricity': {
         info: [{
